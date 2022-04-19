1a0c32532363977a4cf160f51d02f0ed191d456a
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-04-20 07:47:30 +0900

    暗黙的な型解決について

--- pages/index19.tsx
+++ pages/index19.tsx
@@ -6,3 +6,13 @@ function foo<T>(arg: T) {
 };
 const foo1 = foo<number[]>([1,2]);
 const foo2 = foo<string>("hoge");
+
+// 暗黙的な型解決について
+// 引数にnullが入る可能性があるなら型を明示的に書く
+function foo3<T>(arg: T) {
+  return { value: arg} ;
+};
+const foo4 = foo3("");
+const foo5 = foo3(1);
+const foo6 = foo3([false]);
+const foo7 = foo3<string | null>("");
