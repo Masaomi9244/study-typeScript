f3e9da1257c001bf1adb9b42deaed472bd8e5e80
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-04-20 22:45:36 +0900

    inferについて

--- pages/index21.tsx
+++ pages/index21.tsx
@@ -13,3 +13,29 @@ type Filter<T, U> = {
 type StringKeys = Filter<Props, string>; // id | name
 type NumberKeys = Filter<Props, number>; // age
 type BooleanKeys = Filter<Props, Boolean>; // never
+
+// -----------------------------------------------------------------------------------------
+
+// inferとは
+// 部分的な型抽出のこと
+
+// 使用例1 returnの型を取得する
+export const foo = () => {
+  return 0;
+};
+type ReturnType<T> = T extends () => infer U ? U : never; 
+type Foo = ReturnType<typeof foo>; // number
+
+// 使用例2 引数の型を取得する
+export const foo2 = (id: string) => {
+  return 0;
+};
+type ArgType<T> = T extends (id: infer U) => number ? U : never;
+type Foo2 = ArgType<typeof foo2>; // string
+
+// 使用例3　複数の引数の型を取得する ...arg　←　可変長引数
+export const foo3 = (id: string, name: string) => {
+  return 0;
+};
+type MultipleArgType<T> = T extends (...args: infer U) => number ? U : never;
+type Foo3 = MultipleArgType<typeof foo3>; // [id: string, name: string]
