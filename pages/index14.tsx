aa55a166cac727e2d49c704325658891767f79e6
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-05-08 03:14:50 +0900

    インデント調整

--- pages/index14.tsx
+++ pages/index14.tsx
@@ -1,44 +1,63 @@
-// 【TypeScript入門 #14】ダウンキャストやアップキャストとは？またよく使われるas constとは？
-
-// ダウンキャストとアップキャストとは？
-// どちらも型を変える行為を指す
+// 【TypeScript入門 #14】
+// ダウンキャストやアップキャストとは？またよく使われるas constとは？
 
 // ダウンキャスト
-// 型推論で導かれた方が抽象的すぎる場合に、プログラマー側で寄り方を詳しくすること
+// 型推論で導かれた方が抽象的すぎる場合に、プログラマー側でより型を詳しくすること
 // 何かしらの定数ファイルを作る時によく使われる
-export const color = "red" // red
-const theme = {
-  color: "red", // string
-};
-const theme2 = {
-  color: "red" as "red", // red
-};
+namespace Namespace1 {
+  const color = "red" // red
+  const foo = {
+    color: "red", // string
+  }
+
+  const foo2 = {
+    color: "red" as "red", // red
+  }
+}
+
+//-------------------------------------------------------------------------------
 
 // ConstAssertionという書き方
-const theme3 = {
-  color: "red" as const, // red 
-};
+namespace Namespace2 {
+  const foo = {
+    color: "red" as const, // red 
+  }  
+}
+
+//-------------------------------------------------------------------------------
 
 // オブジェクト内のプロパティすべてを指定できる
-const theme4 = {
-  color: "red", // red
-  backgroundColor: "blue" // blue
-} as const; 
+namespace Namespace3 {
+  const foo = {
+    color: "red", // red
+    backgroundColor: "blue" // blue
+  } as const;  
+}
+
+//-------------------------------------------------------------------------------
 
 // Wideningも防げる
-const hoge = "hoge"
-let x = hoge; // string
-const hoge2 = "hoge2" as const
-let y = hoge2;
+namespace Namespace4 {
+  const hoge = "hoge"
+  let x = hoge; // string
+  const hoge2 = "hoge2" as const
+  let y = hoge2; // hoge2
+}
 
-// 実際の使用例（定数ファイル）
-const PATH = {
-  INDEX: '/',
-  LOGIN: '/login',
-  REGISTER: 'register',
-  PROFILE: '/profile',
-} as const;
+//-------------------------------------------------------------------------------
 
+// ダウンキャストの実際の使用例（定数ファイル）
+namespace Namespace5 {
+  const PATH = {
+    INDEX: '/',
+    LOGIN: '/login',
+    REGISTER: 'register',
+    PROFILE: '/profile',
+  } as const;  
+}
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
 // ---------------------------------------------------------------------
 
 // アップキャスト
@@ -47,11 +66,19 @@ const PATH = {
 // 型を自分の力で解決できないときにしかたなく使う
 
 // Non-null assertion
-function getFirstLetter(str?: string) {
-  return str!.charAt(0);
+namespace Namespace6 {
+  function getFirstLetter(str?: string) {
+    return str!.charAt(0);
+  }  
 }
 
+// ---------------------------------------------------------------------
+
 // Double assertion
-function getFirstLetter2(str: number) {
-  return (str as unknown as string).charAt(0);
+namespace Namespace7 {
+  function getFirstLetter(str: number) {
+    return (str as unknown as string).charAt(0);
+  }
 }
+
+export{}
